4e93af3bf1b652114fcdd0feb391a48a76569418
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-11-15 14:14:21 +0530

    add express server with nodemon setup

---
+++ apps/express-backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello world",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3002");
+});
